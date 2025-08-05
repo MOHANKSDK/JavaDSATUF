f37a2816f8569d3e9fe386a17a931a67de82b58e
Mohanraj <Mohan12@gmail.com>
2025-08-05 22:53:13 +0530

    changes made

--- JavaPattern/src/Main.java
+++ JavaPattern/src/Main.java
@@ -8,16 +8,28 @@ public class Main {
         int count=sc.nextInt();
         Main m=new Main();
         System.out.println("Pattern 1");
-        m.Pattern1(count);
+        m.pattern1(count);
         System.out.println("Pattern 2");
-        m.Pattern2(count);
+        m.pattern2(count);
         System.out.println("Pattern 3");
-        m.Pattern3(count);
+        m.pattern3(count);
+        System.out.println("Pattern 4");
+        m.pattern4(count);
+        System.out.println("Pattern 5");
+        m.pattern5(count);
+        System.out.println("Pattern 6");
+        m.pattern6(count);
+        System.out.println("Pattern 7");
+        m.pattern7(count);
+        System.out.println("Pattern 8");
+        m.pattern8(count);
+        System.out.println("Pattern 9");
+        m.pattern9(count);
     }
 
-    public void Pattern1(int count){
+    public void pattern1(int count){
         //1. for the outer loop focus count the no of lines
-        //2. for the outer loop focus on the columns, and convert them somehow to the rows
+        //2. for the inner loop focus on the columns, and convert them somehow to the rows
         //3. print them inside the outer for loop
         //4. observe symmetry [Optional]
 
@@ -37,7 +49,7 @@ public class Main {
         }
     }
 
-    public void Pattern2(int count){
+    public void pattern2(int count){
         //*
         //* *
         //* * *
@@ -52,7 +64,7 @@ public class Main {
         }
     }
 
-    public void Pattern3(int count){
+    public void pattern3(int count){
         //1
         //1 2
         //1 2 3
@@ -66,4 +78,204 @@ public class Main {
             System.out.println("");
         }
     }
+
+    public void pattern4(int count){
+        //1
+        //2 2
+        //3 3 3
+        //4 4 4 4
+        //5 5 5 5 5
+
+
+        for(int i=1;i<=count;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public void pattern5(int count){
+        // * * * * *
+        //* * * *
+        //* * *
+        //* *
+        //*
+
+        for(int i=0;i<count;i++){
+            for(int j=0;j<count-i;j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+
+
+//        for(int i=1;i<=count;i++){
+//            for(int j=0;j<count-i+1;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println("");
+//        }
+    }
+
+    public void pattern6(int count){
+        //1 2 3 4 5
+        //1 2 3 4
+        //1 2 3
+        //1 2
+        //1
+
+//        for(int i=1;i<=count;i++){
+//            for(int j=1;j<=count-i+1;j++){
+//                System.out.print(j+" ");
+//            }
+//            System.out.println("");
+//        }
+
+        for(int i=0;i<count;i++){
+            for(int j=0;j<count-i;j++){
+                System.out.print(j+1+" ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public void pattern7(int count){
+        //        *
+        //      * * *
+        //    * * * * *
+        //  * * * * * * *
+        //* * * * * * * * *
+
+
+        //[4 space,1 star,4 space]
+        //[3 space,3 star,3 space]
+        //[2 space,5 star,2 space]
+        //[1 space,7 star,1 space]
+        //[0 space,9 star,0 space]
+
+        //For space [count-i-1]
+        //For star [2*i+1]
+
+        for(int i=0;i<count;i++){
+            //space
+            for(int j=0;j<count-i-1;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+
+            //space
+            for(int j=0;j<count-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+
+    }
+
+    public void pattern8(int count){
+
+        //* * * * * * * * *
+        //  * * * * * * *
+        //    * * * * *
+        //      * * *
+        //        *
+
+        //[0 space,9 star,0 space]
+        //[1 space,7 star,1 space]
+        //[2 space,5 star,2 space]
+        //[3 space,3 star,3 space]
+        //[4 space,1 star,4 space]
+
+//        for(int i=0;i<count;i++){
+//            //spaces
+//            for(int j=0;j<i;j++){
+//                System.out.print(" ");
+//            }
+//            //stars
+//            for(int j=0;j<2*count-(2*i+1);j++){
+//                System.out.print("*");
+//            }
+//            //spaces
+//            for(int j=0;j<i;j++){
+//                System.out.print(" ");
+//            }
+//            System.out.println("");
+//        }
+
+
+        for(int i=1;i<=count;i++){
+            //spaces
+            for(int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1;j<=2*(count-i)+1;j++){
+                System.out.print("*");
+            }
+            //spaces
+            for(int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public void pattern9(int count){
+
+        //        *
+        //      * * *
+        //    * * * * *
+        //  * * * * * * *
+        //* * * * * * * * *
+        //* * * * * * * * *
+        //  * * * * * * *
+        //    * * * * *
+        //      * * *
+        //        *
+
+        for(int i=0;i<count;i++){
+            //space
+            for(int j=0;j<count-i-1;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+
+            //space
+            for(int j=0;j<count-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+
+        for(int i=0;i<count;i++){
+           //spaces
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=0;j<2*count-(2*i+1);j++){
+                System.out.print("*");
+            }
+            //spaces
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+
+    }
+
+
+
 }
+
+
+
+
